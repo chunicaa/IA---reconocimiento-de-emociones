33fd2ef71ff166e7827bb953eb4ffd96b9f210ac
Marcelo Caamaño <macaamanho@gmail.com>
2025-06-16 20:05:34 -0300

    Update script.js
    prueba video async

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
-const video = document.getElementById('video');
+ video = document.getElementById('video');
 
 Promise.all([
   faceapi.nets.tinyFaceDetector.loadFromUri('models'),
   faceapi.nets.faceExpressionNet.loadFromUri('models')
 ]).then(startVideo);
 
-function startVideo() {
-  navigator.mediaDevices.getUserMedia({ video: {} })
-    .then(stream => video.srcObject = stream)
-    .catch(err => console.error('Error accediendo a la cámara:', err));
+async function startVideo() {
+	await setupCamera(); 
+//  navigator.mediaDevices.getUserMedia({ video: {} })
+//    .then(stream => video.srcObject = stream)
+//    .catch(err => console.error('Error accediendo a la cámara:', err));
 }
 
 video.addEventListener('play', () => {
@@ -28,3 +29,26 @@ video.addEventListener('play', () => {
     faceapi.draw.drawFaceExpressions(canvas, resized);
   }, 100);
 });
+
+async function setupCamera() {
+    // Find the video element on our HTML page
+    //video = document.getElementById('video');
+    
+    // Request the front-facing camera of the device
+    const stream = await navigator.mediaDevices.getUserMedia({
+        'audio': false,
+        'video': {
+  //        facingMode: 'user',
+  //        height: {ideal:1920},
+  //        width: {ideal: 1920},
+        },
+      });
+    video.srcObject = stream;
+    
+    // Handle the video stream once it loads.
+    return new Promise((resolve) => {
+        video.onloadedmetadata = () => {
+            resolve(video);
+        };
+    });
+}
